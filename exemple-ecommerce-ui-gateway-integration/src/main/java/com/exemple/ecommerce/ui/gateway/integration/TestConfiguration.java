b0f2af0166ef3e1b16e42ffe071cf9a0822a8226
doudouchat <38097871+doudouchat@users.noreply.github.com>
2019-12-06 21:37:20 +0100

    module gateway integration (#4)

---
+++ exemple-ecommerce-ui-gateway-integration/src/main/java/com/exemple/ecommerce/ui/gateway/integration/TestConfiguration.java
@@ -0,0 +1,35 @@
+package com.exemple.ecommerce.ui.gateway.integration;
+
+import java.util.logging.Level;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/ws")
+public class TestConfiguration extends ResourceConfig {
+
+    public TestConfiguration() {
+
+        // Test API
+        super.register(TestApi.class)
+
+                // Nom de l'application
+                .setApplicationName("WS Test")
+
+                // logging
+
+                .register(LoggingFeature.class)
+
+                .property(LoggingFeature.LOGGING_FEATURE_VERBOSITY, LoggingFeature.Verbosity.PAYLOAD_ANY)
+
+                .property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL, Level.FINE.getName())
+
+                // JSON
+                .register(JacksonJsonProvider.class);
+
+    }
+
+}
